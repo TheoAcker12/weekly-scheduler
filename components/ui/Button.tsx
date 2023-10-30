aa4bcd4a01085118994bef772c7b491ac173c4f5
Theo Acker <dtheoacker@gmail.com>
2023-10-30 14:30:03 -0700

    improve button options

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,27 +1,53 @@
 import React from "react"
 import styles from "@/styles/ui.module.scss"
+import { AiFillCaretDown, AiFillCaretUp, AiOutlineEdit, AiOutlineClose, AiOutlineSave } from 'react-icons/ai'
+import { FaRegTrashAlt } from 'react-icons/fa'
 import { FiLogOut } from 'react-icons/fi'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  icon?: 'logout',
+  icon?: 'delete' | 'edit' | 'logout' | 'moveUp' | 'moveDown' | 'save' | 'x',
   iconOnly?: boolean, // default false
+  hiddenText?: string, // for assistive technology
+  hiddenTextBeforeRegular?: boolean, // default false
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({icon, iconOnly, ...props}, ref) => {
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({icon, iconOnly, hiddenText, hiddenTextBeforeRegular, ...props}, ref) => {
   let className = props.className;
+
   let iconElement: React.ReactElement|undefined;
   if (icon) {
     if (iconOnly) className += ' ' + styles.iconOnlyBtn;
     else className += ' ' + styles.withIconBtn;
     switch (icon) {
+      case 'delete':
+        iconElement = <FaRegTrashAlt />
+        break;
+      case 'edit':
+        iconElement = <AiOutlineEdit />
+        break;
       case 'logout':
         iconElement = <FiLogOut />
+        break;
+      case 'moveDown':
+        iconElement = <AiFillCaretDown />
+        break;
+      case 'moveUp':
+        iconElement = <AiFillCaretUp />
+        break;
+      case 'save':
+        iconElement = <AiOutlineSave />
+        break;
+      case 'x':
+        iconElement = <AiOutlineClose />
         break;
     }
   }
   else {
     className += ' ' + styles.btn;
   }
+
+  let hiddenElement = <span className="sr-only">{hiddenText}</span>;
+
   return (
     <button
       type="button"
@@ -30,7 +56,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({icon, iconOnly
       className={className}
     >
       {iconElement ? iconElement : ''}
+      {hiddenText && hiddenTextBeforeRegular ? hiddenElement : ''}
       {props.children}
+      {hiddenText && !hiddenTextBeforeRegular ? hiddenElement : ''}
     </button>
   )
 })
