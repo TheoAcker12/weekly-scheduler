6ab7d91b30ebf4d63579fcf5a128e35ea1abed59
Theo Acker <dtheoacker@gmail.com>
2023-11-02 11:38:52 -0700

    add filters to sort and filter form

--- components/sort-and-filter/SortAndFilterForm.tsx
+++ components/sort-and-filter/SortAndFilterForm.tsx
@@ -1,4 +1,5 @@
 import { Select } from '@/components/ui/Select';
+import { Button } from '@/components/ui/Button';
 import CustomLink from '@/components/ui/CustomLink';
 import styles from '@/styles/filter-form.module.scss'
 import { ParsedUrlQuery } from 'querystring';
@@ -6,21 +7,76 @@ import { Category } from '@/lib/api_schema';
 import { useImmerReducer } from 'use-immer';
 import { FilterAction, FilterState, filterReducer } from './filters-reducer';
 import { parseSortParam } from '@/lib/utils';
+import { Filter } from '@/lib/types';
 
 type Props = {
   params: ParsedUrlQuery,
   categories: Category[],
 }
 
+/**
+ * get a list of category options for a given filter - cannot include any categories selected in other filters (basically, narrow down the options provided by checking other filters)
+ * 
+ * @param index number: the index of the filter to provide the options for
+ * @param catOptions list of elements: List of options - one for each category, in the same order as the categories list
+ * @param filters Filter[]: all current filters, filters[index] is the one we are looking at
+ * @returns list of category options to select from - will include the currently selected category, but not any categories selected by other filters
+ */
+function getFilterCatOptions(index: number, catOptions: JSX.Element[], filters: Filter[]): JSX.Element[] {
+  // list all selected categories, excluding this filter (as defined by index)
+  const categories: number[] = filters.reduce<number[]>((list, filter, i) => {
+    if (filter.cat_index !== -1 && index !== i) list.push(filter.cat_index);
+    return list;
+  }, [])
+  // return only options that have not been included in the above list
+  return catOptions.filter((o, i) => !categories.includes(i));
+}
+
+/**
+ * get a list of field options for a given filter - determine correct default option and potential fields depending on currently selected category
+ * 
+ * @param filter Filter: the filter we care about
+ * @param categories Category[]: list of all categories from database
+ * @returns list of field options to select from - will include a default option, and, if a category is selected, options to add all fields, remove all fields, and toggle each field
+ */
+function getFilterFieldOptions(filter: Filter, categories: Category[]): JSX.Element[] {
+  // valid category selected?
+  if (filter.cat_index === -1) return [<option key='default' value='default'>No category selected</option>];
+  // empty arrays to place options for adding and removing fields into
+  const addOpts: JSX.Element[] = [];
+  const removeOpts: JSX.Element[] = [];
+  // determine which fields can be added vs. removed
+  const cat = categories[filter.cat_index];
+  for (let i = 0; i < cat.fields.length; i++) {
+    const add = !filter.field_index_list.includes(i);
+    const option = <option key={i} value={i}>{add ? 'Add' : 'Remove'} {cat.fields[i].name}</option>
+    if (add) addOpts.push(option);
+    else removeOpts.push(option);
+  }
+  // add optgroups (if applicable)
+  const options = [<option key='default' value='default'>Select an action...</option>];
+  if (addOpts.length) options.push(<optgroup label='Add' key='add'>{addOpts}</optgroup>);
+  if (removeOpts.length) options.push(<optgroup label='Remove' key='remove'>{removeOpts}</optgroup>);
+  // add standard options (if applicable)
+  if (addOpts.length) options.push(<option key='addAllFields' value='addAllFields'>Add all fields</option>);
+  if (removeOpts.length) options.push(<option key='clearAllFields' value='clearAllFields'>Remove all fields</option>);
+  return options;
+}
+
 export default function SortAndFilterForm({ categories, ...props}: Props) {
-  console.log(props.params['sort_by']);
-  console.log(parseSortParam(props.params['sort_by'], categories))
   const [state, dispatch] = useImmerReducer<FilterState, FilterAction>(filterReducer,  {
     sortIndex: parseSortParam(props.params['sort_by'], categories),
+    filters: [],
   });
 
   // for use populating sort and filter category select options
   const catOptions = categories.map((cat, index) => <option key={index} value={index}>{cat.name}</option>);
+
+  // button may show up in one of two different places, so defining it outside of the return statement
+  const addFilterBtn = <Button
+    id='add-filter-btn'
+    onClick={() => dispatch({type: 'form/filterAdded'})}
+  >Add Filter</Button>
 
   return (
     <fieldset>
@@ -38,8 +94,60 @@ export default function SortAndFilterForm({ categories, ...props}: Props) {
       <hr />
       <div className='filters-summary'>
         <span>Filter by: fiter summary goes here</span>
+        {state.filters.length > 0 ? '' : addFilterBtn}
       </div>
       <div className={styles.filterList}>
+        
+      {state.filters.length > 0 ?
+        <ul>
+          {state.filters.map((f, index) => 
+            <li key={index}>
+              <div className={styles.filter}>
+                <Select
+                  id={`filter-${index}-cat-select`}
+                  labelText='Filter by: '
+                  value={f.cat_index > -1 ? f.cat_index : 'default'}
+                  onChange={(e) => dispatch({type: 'filter/categorySelected', index, categories, value: e.target.value})}
+                  defaultOption="Select a category..."
+                >
+                  {getFilterCatOptions(index, catOptions, state.filters)}
+                </Select>
+                <Select
+                  id={`filter-${index}-type-select`}
+                  value={f.include ? 'include' : 'exclude'}
+                  onChange={(e) => dispatch({type: 'filter/includeSelected', index, value: e.target.value})}
+                  labelText='Type of filter'
+                  hideLabel={true}
+                >
+                  <option value='include'>Include</option>
+                  <option value='exclude'>Exclude</option>
+                </Select>
+                <span>Fields: {f.field_index_list.map((i) => categories[f.cat_index].fields[i].name).join(', ')}</span>
+                <Select
+                  id={`filter-${index}-fields-select`}
+                  value='default'
+                  onChange={(e) => dispatch({type: 'filter/fieldSelected', index, categories, value: e.target.value})}
+                  disabled={f.cat_index === -1}
+                  labelText='Add or remove fields: '
+                  hideLabel={true}
+                >
+                  {getFilterFieldOptions(f, categories)}
+                </Select>
+                <Button
+                  id={`filter-${index}-delete-btn`}
+                  icon='delete'
+                  iconOnly={true}
+                  className="filter-delete-btn"
+                  onClick={() => dispatch({type: 'form/filterDeleted', index})}
+                  hiddenText="Delete filter"
+                />
+              </div>
+            </li>
+          )}
+          {/* only include add filter button if there are still categories left for filtering */}
+          {getFilterCatOptions(-1, catOptions, state.filters).length ? <li className={styles.addFilterListItem}>{addFilterBtn}</li> : ''}
+        </ul>
+        : ''}
       </div>
       <hr />
       <div>
--- components/sort-and-filter/filters-reducer.ts
+++ components/sort-and-filter/filters-reducer.ts
@@ -1,11 +1,18 @@
 import { Category } from "@/lib/api_schema"
+import { Filter } from "@/lib/types";
 
 export type FilterState = {
   sortIndex?: number,
+  filters: Filter[],
 }
 
 export type FilterAction =
 | {type: 'form/sortCategorySelected', value: string, categories: Category[]}
+| {type: 'form/filterAdded'}
+| {type: 'form/filterDeleted', index: number}
+| {type: 'filter/includeSelected', index: number, value: string}
+| {type: 'filter/categorySelected', index: number, value: string, categories: Category[]}
+| {type: 'filter/fieldSelected', index: number, value: string, categories: Category[]}
 
 export function filterReducer(state: FilterState, action: FilterAction): void {
   switch (action.type) {
@@ -16,5 +23,57 @@ export function filterReducer(state: FilterState, action: FilterAction): void {
       else state.sortIndex = index;
       break;
     }
+    case 'form/filterAdded':
+      state.filters.push({include: true, cat_index: -1, field_index_list: []});
+      break;
+    case 'form/filterDeleted':
+      if (state.filters[action.index]) state.filters.splice(action.index, 1);
+      break;
+    default:
+      // filter index required for all other actions
+      const index = action.index;
+      if (!state.filters[index]) break;
+      switch (action.type) {
+        case 'filter/includeSelected':
+          state.filters[index].include = action.value === 'include';
+          break;
+        case 'filter/categorySelected': {
+          // validate category index
+          const cat_index = parseInt(action.value);
+          if (isNaN(cat_index) || !action.categories[cat_index]) {
+            state.filters[index].cat_index = -1;
+            // cannot have any fields set because no valid category to select fields from
+            state.filters[index].field_index_list = [];
+          }
+          else if (cat_index !== state.filters[index].cat_index) {
+            state.filters[index].cat_index = cat_index;
+            // reset fields because a new category has been selected
+            state.filters[index].field_index_list = [];
+          }
+          break;
+        }
+        case 'filter/fieldSelected': {
+          const filter = state.filters[index];
+          if (filter.cat_index === -1) break; // must have valid category set
+          // handle valid options
+          const category = action.categories[filter.cat_index];
+          switch (action.value) {
+            case 'addAllFields':
+              state.filters[index].field_index_list = Array.from(category.fields.keys());
+              break;
+            case 'clearAllFields':
+              state.filters[index].field_index_list = [];
+              break;
+            default:
+              // validate field index
+              const field_index = parseInt(action.value);
+              if (isNaN(field_index) || !category.fields[field_index]) break;
+              // field should be included if it was already in the list or if it is the selected field, but not both (i.e. if the selected field is already in the list, remove it)
+              state.filters[index].field_index_list = Array.from(category.fields.keys()).filter((i) => (filter.field_index_list.includes(i)) !== (i === field_index));
+              break;
+          }
+          break;
+        }
+      }
   }
 }
