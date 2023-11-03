86c0c93ca12ceea6d956261bfbefd46dce349344
Theo Acker <dtheoacker@gmail.com>
2023-11-03 16:48:56 -0700

    update custom link so that custom class names are only set on outer element and not also on anchor element

--- components/ui/CustomLink.tsx
+++ components/ui/CustomLink.tsx
@@ -7,19 +7,19 @@ import styles from "@/styles/ui.module.scss"
 
 type LinkProps = IconProps & React.ComponentProps<typeof Link>;
 
-export default function CustomLink({icon, iconOnly, hiddenText, hiddenTextBeforeRegular, children, ...props}: LinkProps) {
-  let className = props.className ?? '';
+export default function CustomLink({icon, iconOnly, hiddenText, hiddenTextBeforeRegular, children, className, ...props}: LinkProps) {
+  let newClassName = className ?? '';
 
   if (icon) {
-    if (iconOnly) className += ' ' + styles.iconOnlyLink;
-    else className += ' ' + styles.withIconLink;
+    if (iconOnly) newClassName += ' ' + styles.iconOnlyLink;
+    else newClassName += ' ' + styles.withIconLink;
   }
-  else className += ' ' + styles.btnLink;
+  else newClassName += ' ' + styles.btnLink;
 
   const iconProps = {icon, iconOnly, hiddenText, hiddenTextBeforeRegular};
 
   return (
-    <div className={className}>
+    <div className={newClassName}>
       <Link {...props}>
         <ChildrenWithIcon {...iconProps}>{children}</ChildrenWithIcon>
       </Link>
